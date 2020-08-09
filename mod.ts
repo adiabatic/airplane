9e7d5d72718428bce741e1f86be26d040ee14fec
Nathan Galt <adiabatic@users.noreply.github.com>
2020-08-09 10:05:55 -0700

    Start sending out feeds

--- mod.ts
+++ mod.ts
@@ -1,9 +1,18 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
+import Feed from './json_feed.ts';
 
 const app = new Application();
 
+const aFeed: Feed = {
+  version: "https://jsonfeed.org/version/1.1",
+  title: 'Here Comes the Airplane',
+  items: [
+
+  ]
+}
+
 app.use((ctx) => {
-  ctx.response.body = "Hello world!";
+  ctx.response.body = aFeed;
 });
 
 const SERVER = "127.0.0.1";
