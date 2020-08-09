0a96bb732b2010368e6b59ad6f68291bb2520dab
Nathan Galt <adiabatic@users.noreply.github.com>
2020-08-09 12:04:26 -0700

    Add item, change port

--- mod.ts
+++ mod.ts
@@ -1,22 +1,29 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
-import Feed from './json_feed.ts';
+import { Application } from "https://deno.land/x/oak/mod.ts"; // probably 4.0, but I'm not picky yet
+import Feed from "./json_feed.ts";
 
 const app = new Application();
 
 const aFeed: Feed = {
   version: "https://jsonfeed.org/version/1.1",
-  title: 'Here Comes the Airplane',
+  title: "Here Comes the Airplane",
   items: [
-
-  ]
-}
+    {
+      id: "d2f2aef9-3227-48c5-a8ba-be7ac2c63a2f",
+      title: "First post!",
+      content_text: "This is my first post.",
+    },
+  ],
+};
 
 app.use((ctx) => {
+  const accepts = ctx.request.accepts()
+  console.log(accepts);
+  
   ctx.response.body = aFeed;
 });
 
 const SERVER = "127.0.0.1";
-const PORT = 8000;
+const PORT = 3000;
 const LISTEN_TARGET = `${SERVER}:${PORT}`;
 console.log(`Listening on <http://${LISTEN_TARGET}>`);
 
