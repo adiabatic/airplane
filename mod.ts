6b2bafad0b44507b6e61b35d4b0cf980bc8b889b
Nathan Galt <adiabatic@users.noreply.github.com>
2020-08-09 09:20:53 -0700

    Initial commit

---
+++ mod.ts
@@ -0,0 +1,14 @@
+import { Application } from "https://deno.land/x/oak/mod.ts";
+
+const app = new Application();
+
+app.use((ctx) => {
+  ctx.response.body = "Hello world!";
+});
+
+const SERVER = "127.0.0.1";
+const PORT = 8000;
+const LISTEN_TARGET = `${SERVER}:${PORT}`
+console.log(`Listening on <http://${LISTEN_TARGET}>`);
+
+await app.listen(LISTEN_TARGET);
